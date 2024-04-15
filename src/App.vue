f3ba7321c03307d5b5b927cef89b73a05477a8cd
Salvodroches <mr.umar1998@gmail.com>
2024-04-15 14:22:07 +0500

    Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+//-4161185926
+//7168094994:AAGDxeO3mZds5N5aIVcs3kw_J1eSjJJkhm0
+
+
+
+import VHeader from "@/components/VHeader.vue";
+import VFooter from "@/components/VFooter.vue";
+import VPromo from "@/components/VPromo.vue";
+import VProjects from "@/components/VProjects.vue";
+import VTech from "@/components/VTech.vue";
+import VFeedback from "@/components/VFeedback.vue";
+</script>
+
+<template>
+  <v-header/>
+  <main>
+    <v-promo/>
+    <v-projects/>
+    <v-tech/>
+    <v-feedback/>
+
+  </main>
+  <v-footer/>
+</template>
+
+<style scoped>
+
+</style>
